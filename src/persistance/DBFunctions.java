15598539e7a33bc61b5563b1c84efcd226e222a8
faileon <pt4cekt@gmail.com>
2015-03-02 19:15:36 +0100

    dbfunctions

--- src/persistance/DBFunctions.java
+++ src/persistance/DBFunctions.java
@@ -1,6 +1,11 @@
 package persistance;
 
 import javaproject.Player;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -9,7 +14,32 @@ import java.util.ArrayList;
 public class DBFunctions {
 
     public ArrayList<Player> getPlayers(){
+        ArrayList<Player> players;
         String select = "SELECT * FROM Player";
+
+        //Querries:
+        Statement statement;
+        Connection connection = DBConnection.getConnection();
+        try{
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(select);
+
+            while(resultSet.next()){
+                int playerID = resultSet.getInt("playerID");
+                String name = resultSet.getString("name");
+                String surname = resultSet.getString("surname");
+                String password = resultSet.getString("password");
+                String nickname = resultSet.getString("nickname");
+                Boolean admin_rights = resultSet.getBoolean("admin_rights");
+
+                Player player = new Player(playerID ,name, surname, password, nickname, admin_rights);
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        } {
+
+        }
 
         return null;
     }
