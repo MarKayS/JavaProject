92f598a5d8d25435e4b1f7d1f5dc010da7474b92
faileon <pt4cekt@gmail.com>
2015-03-02 18:44:48 +0100

    new persisance DB

--- src/persistance/DBConnection.java
+++ src/persistance/DBConnection.java
@@ -8,23 +8,28 @@ import java.sql.Connection;
 public class DBConnection {
 
     // database URL
-    private static final String dbURL = "jdbc:mysql://wm48.wedos.net/d24820_jsoko";
+    private static final String dbURL = "jdbc:mysql://wm48.wedos.net:3306";
     private static final String username = "a24820_jsoko";
     private static final String password = "sokoban123";
-    private static final String driver = "com.mysql.jdbc.Driver";
     // declare Connection for accessing and querying database
     private Connection connection;
 
     // constructor connects to database
-    public DBConnection() throws ClassNotFoundException {
+    public DBConnection() {
         // connect to database books
+        try {
 
-        // establish connection to database
-        connection = null;
-        Class.forName("com.mysql.jdbc.Driver");
+            // establish connection to database
+            connection = (Connection) DriverManager.getConnection(dbURL, username, password);
+
+        } catch (SQLException sqlException) {
+            JOptionPane.showMessageDialog(null, sqlException.getMessage(),
+                    "Database Error", JOptionPane.ERROR_MESSAGE);
+
+            System.exit(1);
+        }
+
     }
-           // connection = (Connection) DriverManager.getConnection(dbURL, username, password);
-
 
     public void closeConnection() {
         try {
