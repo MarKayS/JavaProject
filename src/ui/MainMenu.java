ef762920285dacd534c032845f2775c7319f6133
MarKay <marekkajfosz@gmail.com>
2015-03-26 17:14:50 +0100

    input for screen 4 fixed

--- src/ui/MainMenu.java
+++ src/ui/MainMenu.java
@@ -406,7 +406,7 @@ public class MainMenu extends BasicGameState {
                 }
                 break;
             case 4:
-                for (int i = 6; i <= 10; i++) {
+                for (int i = 3; i <= 5; i++) {
                     moas.get(i).setAcceptingInput(true);
                 }
                 break;
